2e71d62661632820035779134f154fe044a8a88c
utkarshsurwade <utkarsh77777@gmail.com>
2016-12-25 13:54:34 +0530

    modifications

--- 008_packages/main_code/demo.java
+++ 008_packages/main_code/demo.java
@@ -1,4 +1,5 @@
 import p1.*;
+import p1.p2.*;
 class demo 
 {
  public static void main(String args[])
@@ -10,5 +11,9 @@ class demo
  Xyz q=new Xyz();
  q.get();
  q.sub();
+ 
+ Pqr r=new Pqr();
+ r.get();
+ r.mul();
  }
 }
