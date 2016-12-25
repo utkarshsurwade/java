2bd4b9c6c48c09a15b6e0b4194c3a362195cc2de
utkarshsurwade <utkarsh77777@gmail.com>
2016-12-25 13:57:14 +0530

    modificationss

--- 005_interface/Test.java
+++ 005_interface/Test.java
@@ -1,7 +1,8 @@
  interface Info
 {
- abstract void display();
- abstract void show();
+ float pi=3.14f;//no need to write final cuz its already final
+ void display();
+ void show();
 }
 
 
---
+++ 008_packages/just_another_folder/just_another_another_folder/p1/p2/Pqr.java
@@ -0,0 +1,15 @@
+package p1.p2;
+public class Pqr
+{
+ private int a,b;
+ public void mul()
+ {
+ System.out.println(a*b);
+ }
+
+ public void get()
+ { 
+ a=20;
+ b=10;
+ }
+}
