1aa7b0cf275fbb48bb823a26c76fab7d043eadc5
utkarshsurwade <utkarsh77777@gmail.com>
2016-12-31 12:18:39 +0530

    new

---
+++ 005_interface/Info.java
@@ -0,0 +1,7 @@
+ interface Info
+{
+ float pi=3.14f;//no need to write final cuz its already final
+ void display();
+ void show();
+}
+
--- 005_interface/Test.java
+++ 005_interface/Test.java
@@ -1,11 +1,3 @@
- interface Info
-{
- float pi=3.14f;//no need to write final cuz its already final
- void display();
- void show();
-}
-
-
 class Demo implements Info
 {
  
